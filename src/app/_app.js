50e1bfceb4069e4252d52c63dac566979ece73db
Syntaxri <kssakram92@gmail.com>
2026-03-24 19:33:25 +0100

    fix: favicon

--- src/app/_app.js
+++ src/app/_app.js
@@ -8,8 +8,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         {/* ── Primary favicon ── */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
